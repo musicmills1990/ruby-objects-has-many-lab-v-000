3f5c2bccc39660fc8b0076daeab1f2cc75813698
Marc Mills <mdmills1990@gmail.com>
2018-09-07 14:16:43 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+attr_accessor :author, :title
+@@all = []
+
+def initialize(title)
+@title = title
+@@all << self
+end
+
+def author_name
+    if self.author == nil
+      nil
+    else
+      self.author.name
+    end
+end
+
+def self.all
+  @@all
+end
+
+end
